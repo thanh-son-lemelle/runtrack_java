3c60b26cbeeecb74981c6f8826e2820c233038b5
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-06-10 13:08:09 +0200

    first commit

---
+++ jour01/HelloWorld.java
@@ -0,0 +1,8 @@
+package jour01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello world" to the terminal
+        System.out.println("Hello world"); // This prints the message to the console
+    }
+}
